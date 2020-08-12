bb70fbc630ee8bfd943ca90cd7d3755734698a2c
j3ffh95 <j3ffh95@gmail.com>
2020-08-11 22:08:55 -0400

    Added event listeners

--- main.js
+++ main.js
@@ -5,5 +5,13 @@ for (let i = 0; i < SQUARES_NR; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
 
+  square.addEventListener("mouseover", () => {
+    setColorToEl(square);
+  });
+
+  square.addEventListener("mouseout", () => {
+    removeColorFromEl(square);
+  });
+
   container.appendChild(square);
 }
