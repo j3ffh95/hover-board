7909e70786212b8e46db1d0817e8f3b125bf9f73
j3ffh95 <j3ffh95@gmail.com>
2020-08-11 22:02:30 -0400

    Created for loop to create squares

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const container = document.querySelector("#container");
+const SQUARES_NR = 500;
+
+for (let i = 0; i < SQUARES_NR; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  container.appendChild(square);
+}
