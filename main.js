45d9a7c15d2c06f205ffa311994d81419d848366
j3ffh95 <j3ffh95@gmail.com>
2020-08-11 22:14:19 -0400

    Make the buttons light up adding box shadow style

--- main.js
+++ main.js
@@ -71,6 +71,7 @@ for (let i = 0; i < SQUARES_NR; i++) {
 function setColorToEl(element) {
   const color = getRandomColor();
   element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 function getRandomColor() {
